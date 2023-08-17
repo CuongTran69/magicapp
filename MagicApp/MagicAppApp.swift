178a8f4cd350e9c9b4d306c5a31c9aef1925fdad
Cường Trần <cuongtv91@ghtk.co>
2023-08-17 15:25:53 +0700

    Initial Commit

---
+++ MagicApp/MagicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicAppApp.swift
+//  MagicApp
+//
+//  Created by Cường Trần on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MagicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
